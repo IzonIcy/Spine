95116d9c03045a31b42913583025d8161cb6a0cd
ryan <ryanbahadori@macmini.mynetworksettings.com>
2026-07-31 19:27:43 -0700

    chore: stop tracking build artifacts in target/
    The entire target/ directory (1684 files of compiled build output)
    was committed by accident. It's already in .gitignore, so this just
    removes it from version control.

--- target/debug/deps/stability-a0abcb424ce1ea2e.d
+++
@@ -1,6 +0,0 @@
-/Users/ryanbahadori/Documents/GitHub/Spine/target/debug/deps/stability-a0abcb424ce1ea2e.d: /Users/ryanbahadori/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/stability-0.2.1/src/lib.rs /Users/ryanbahadori/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/stability-0.2.1/src/unstable.rs
-
-/Users/ryanbahadori/Documents/GitHub/Spine/target/debug/deps/libstability-a0abcb424ce1ea2e.dylib: /Users/ryanbahadori/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/stability-0.2.1/src/lib.rs /Users/ryanbahadori/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/stability-0.2.1/src/unstable.rs
-
-/Users/ryanbahadori/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/stability-0.2.1/src/lib.rs:
-/Users/ryanbahadori/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/stability-0.2.1/src/unstable.rs:
